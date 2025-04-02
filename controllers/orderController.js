2245eed8a3bceccaf33621bedbb697be3d2c8d21
DinhManh203 <dinhmanhoke203@gmail.com>
2025-04-03 03:14:10 +0700

    10th_update

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -52,21 +52,65 @@ const placeOrderRazorpay = async (req, res) => {
 //All Orders data for Admin Panel
 const allOrders = async (req, res) => {
 
+    try {
 
+        const orders = await orderModel.find({})
+        res.json({
+            success: true,
+            orders
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false, 
+            message: error.message
+        })
+    }
 
 }
 
 //User Order Data For Frontend
 const userOrders = async (req, res) => {
 
+    try {
 
+        const { userId } = req.body
+
+        const orders = await orderModel.find({ userId })
+        res.json({ 
+            success: true,
+            orders
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false, 
+            message: error.message
+        })
+    }
 
 }
 
 //update order status from Admin Panel
 const updateStatus = async (req, res) => {
 
+    try {
 
+        const { orderId, status } = req.body
+
+        await orderModel.findByIdAndUpdate(orderId, {status})
+        res.json({
+            success: true,
+            message: 'Status Updated'
+        })
+        
+    } catch (error) {
+
+
+        
+    }
 
 }
 
